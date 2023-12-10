8dba100e5161c1455a87a322badf870ecbee8514
Прокофьев Андрей <pr0kofyew@yandex.ru>
2023-12-10 18:40:51 +0300

    ready

---
+++ ya_news/news/conftest.py
@@ -0,0 +1,82 @@
+from datetime import datetime, timedelta
+
+import pytest
+from django.conf import settings
+from django.utils import timezone
+from news.models import Comment, News
+
+
+@pytest.fixture
+def author(django_user_model):
+    """Автор комментария."""
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def author_client(author, client):
+    """Залогиненный автор комментария."""
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def news():
+    """Данные новости."""
+    news = News.objects.create(
+        title='Заголовок новости',
+        text='Текст новости'
+    )
+    return news
+
+
+@pytest.fixture
+def pk_news(news):
+    return (news.pk,)
+
+
+@pytest.fixture
+def comment(author, news):
+    """Данные комментария."""
+    comment = Comment.objects.create(
+        news=news,
+        author=author,
+        text='Текст комментария'
+    )
+    return comment
+
+
+@pytest.fixture
+def news_list():
+    """Список новостей."""
+    today = datetime.today()
+    news_list = News.objects.bulk_create(
+        News(
+            title=f'Заголовок новости {index}',
+            text=f'Текст новости {index}',
+            date=today - timedelta(days=index)
+        )
+        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+    )
+    return news_list
+
+
+@pytest.fixture
+def comments_list(author, news):
+    """Список комментариев."""
+    for index in range(3):
+        comment = Comment.objects.create(
+            news=news,
+            author=author,
+            text=f'Текст комментария {index}.'
+        )
+        comment.created = timezone.now() + timedelta(days=index)
+        comment.save()
+    return comments_list
+
+
+@pytest.fixture
+def form_data():
+    """Форма с данными нового комментария.."""
+    return {
+        'text': 'Новый комментарий'
+    }
