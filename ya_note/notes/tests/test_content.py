aaad51b23c5c61f95cc37f4097ee19f6ab03f08f
Прокофьев Андрей <pr0kofyew@yandex.ru>
2023-12-15 11:50:25 +0300

    now ok

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 from notes.forms import NoteForm
-
 from .mixin import TestMixinAuthorNoteReader, TestMixinCreatNoteConstant
 
 User = get_user_model()
